01de76ffeacf1ef20a5f219522671130dad1b988
João Pedro Paz <77215433+JoaoPedroPaz@users.noreply.github.com>
2023-06-01 19:37:09 -0300

    v0.2    Button Setup

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,16 @@
 // ----------------------------------------------------------------------------
 
 #define LED_PIN 26
+#define BTN_PIN 22
 
 // ----------------------------------------------------------------------------
-// Definition of the Led component
+// Definition of global constants
+// ----------------------------------------------------------------------------
+
+const uint8_t DEBOUNCE_DELAY = 10; // in milliseconds
+
+// ----------------------------------------------------------------------------
+// Definition of the LED component
 // ----------------------------------------------------------------------------
 
 struct Led {
@@ -30,26 +37,76 @@ struct Led {
 };
 
 // ----------------------------------------------------------------------------
+// Definition of the Button component
+// ----------------------------------------------------------------------------
+
+struct Button {
+    // state variables
+    uint8_t  pin;
+    bool     lastReading;
+    uint32_t lastDebounceTime;
+    uint16_t state;
+
+    // methods determining the logical state of the button
+    bool pressed()                { return state == 1; }
+    bool released()               { return state == 0xffff; }
+    bool held(uint16_t count = 0) { return state > 1 + count && state < 0xffff; }
+
+    // method for reading the physical state of the button
+    void read() {
+        // reads the voltage on the pin connected to the button
+        bool reading = digitalRead(pin);
+
+        // if the logic level has changed since the last reading,
+        // we reset the timer which counts down the necessary time
+        // beyond which we can consider that the bouncing effect
+        // has passed.
+        if (reading != lastReading) {
+            lastDebounceTime = millis();
+        }
+
+        // from the moment we're out of the bouncing phase
+        // the actual status of the button can be determined
+        if (millis() - lastDebounceTime > DEBOUNCE_DELAY) {
+            // don't forget that the read pin is pulled-up
+            bool pressed = reading == LOW;
+            if (pressed) {
+                     if (state  < 0xfffe) state++;
+                else if (state == 0xfffe) state = 2;
+            } else if (state) {
+                state = state == 0xffff ? 0 : 0xffff;
+            }
+        }
+
+        // finally, each new reading is saved
+        lastReading = reading;
+    }
+};
+
+// ----------------------------------------------------------------------------
 // Definition of global variables
 // ----------------------------------------------------------------------------
 
-Led led = { LED_PIN, false };
+Led    led    = { LED_PIN, false };
+Button button = { BTN_PIN, HIGH, 0, 0 };
 
 // ----------------------------------------------------------------------------
 // Initialization
 // ----------------------------------------------------------------------------
 
 void setup() {
-    pinMode(led.pin, OUTPUT);
+    pinMode(led.pin,    OUTPUT);
+    pinMode(button.pin, INPUT);
 }
 
 // ----------------------------------------------------------------------------
 // Main control loop
 // ----------------------------------------------------------------------------
 
-
 void loop() {
-    led.on = millis() % 1000 < 50;
-    led.update();
+    button.read();
 
+    if (button.pressed()) led.on = !led.on;
+
+    led.update();
 }
